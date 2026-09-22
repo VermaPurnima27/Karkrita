b001f1138e9d491b41f412d214236bfb5f8c4cf6
Purnima Verma <vpurvi26@gmail.com>
2026-09-22 22:50:15 +0530

    signupapi creation

--- backend/server.js
+++ backend/server.js
@@ -2,17 +2,29 @@ const express = require("express");
 const cors = require("cors");
 require("dotenv").config();
 
+const connectDB = require("./config/db");
+const authRoutes = require("./routes/authRoutes");
+
 const app = express();
 
+// Connect MongoDB
+connectDB();
+
+// Middleware
 app.use(cors());
 app.use(express.json());
 
+// Auth Routes
+app.use("/api/auth", authRoutes);
+
+// Test Route
 app.get("/", (req, res) => {
   res.send("Karkrita Backend is running!");
 });
 
+// Server
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
-  console.log(`Karkrita Backend running on port http://localhost:${PORT}`);
+  console.log(`Karkrita Backend running on port ${PORT}`);
 });
